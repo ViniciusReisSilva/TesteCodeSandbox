bc443e602bf4d60611e71a1a6a9c21c84c8a73f8
ViniciusReisSilva <viniciusreis20111@hotmail.com>
2025-08-29 23:17:54 -0300

    incluindo mais uma linha no arquivo app,js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 console.log("Rodando app e fazendo testes com Git e Github")
 console.log("Aplicação está se conectando com o banco de dados")
+consloe.log("Incluinddo uma nova linha pela branch desenvolvimento")
